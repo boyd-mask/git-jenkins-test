61b5177a44f902f6dd72b2f8023b2d8802cd66aa
Byambasuren Ganbaatar <byambaa.mng@gmail.com>
2016-08-30 16:07:00 +0900

    issue_num get

--- fabfile.py
+++ fabfile.py
@@ -8,4 +8,5 @@ def test():
     f.close()
     comment = json.dumps({'body': text})
 
-    local("curl https://api.github.com/repos/boyd-mask/git-jenkins-test/issues/2/comments -X POST -d \'{0}\'".format(comment))
+
+    # local("curl https://api.github.com/repos/boyd-mask/git-jenkins-test/issues/2/comments -X POST -d \'{0}\'".format(comment))
